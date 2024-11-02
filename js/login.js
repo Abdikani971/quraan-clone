f128b8eecc3e6a7fd94c2452fb705c25ea3e02c3
Abdikani971 <abdikanimohamed00971@gmail.com>
2024-11-02 21:28:00 +0300

    check if the user is already logged in

--- js/login.js
+++ js/login.js
@@ -4,4 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
   messageDiv.style.color = "red";
   messageDiv.style.marginTop = "10px";
   loginForm.appendChild(messageDiv);
+
+  if (localStorage.getItem("isLoggedIn")) {
+    messageDiv.textContent = "You are already logged in!";
+    setTimeout(() => {
+      window.location.href = "/index.html";
+    }, 10);
+  }
 });
