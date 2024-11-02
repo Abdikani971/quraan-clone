026094d757ee32d7531c6d96b17b25bb53824b95
Abdikani971 <abdikanimohamed00971@gmail.com>
2024-11-02 21:29:40 +0300

    add user verification logic for login

--- js/login.js
+++ js/login.js
@@ -16,5 +16,18 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault();
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
+
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user && user.email === email && user.password === password) {
+      localStorage.setItem("isLoggedIn", true);
+      messageDiv.style.color = "green";
+      messageDiv.textContent = "Login successful! Redirecting...";
+      setTimeout(() => {
+        window.location.href = "/index.html";
+      }, 2000);
+    } else {
+      messageDiv.textContent = "Invalid email or password.";
+    }
   });
 });
