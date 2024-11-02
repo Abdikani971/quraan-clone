c15a77aae966eb868e5a68cd904c462092fe732f
Abdikani971 <abdikanimohamed00971@gmail.com>
2024-11-02 21:36:42 +0300

    display buttons conditionally based on login status

--- js/auth.js
+++ js/auth.js
@@ -2,4 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const logoutButton = document.getElementById("logout-button");
   const loginButton = document.getElementById("login-button");
   const registerButton = document.getElementById("register-button");
+
+  if (localStorage.getItem("isLoggedIn")) {
+    loginButton.style.display = "none";
+    registerButton.style.display = "none";
+    logoutButton.style.display = "inline";
+  } else {
+    logoutButton.style.display = "none";
+  }
 });
