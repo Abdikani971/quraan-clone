ee3c3a714f18677455d3feabad96c8eaa21406ae
Abdikani971 <abdikanimohamed00971@gmail.com>
2024-11-02 21:18:34 +0300

    implement check for existing login session

--- js/register.js
+++ js/register.js
@@ -4,4 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
   messageDiv.style.color = "red";
   messageDiv.style.marginTop = "10px";
   registerForm.appendChild(messageDiv);
+
+  if (localStorage.getItem("isLoggedIn")) {
+    messageDiv.textContent = "You are already registered and logged in!";
+    setTimeout(() => {
+      window.location.href = "/index.html";
+    }, 10);
+    return;
+  }
 });
