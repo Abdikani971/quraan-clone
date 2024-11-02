65e45ee0101ba7c4e1def69e7c4281268317cc0c
Abdikani971 <abdikanimohamed00971@gmail.com>
2024-11-02 21:35:43 +0300

    add DOMContentLoaded event listener to handle button visibility

---
+++ js/auth.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const logoutButton = document.getElementById("logout-button");
+  const loginButton = document.getElementById("login-button");
+  const registerButton = document.getElementById("register-button");
+});
