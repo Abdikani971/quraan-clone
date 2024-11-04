3e216b64f39cef5e8fd6493782dc528a80f918e1
Abdikani971 <abdikanimohamed00971@gmail.com>
2024-11-04 11:56:13 +0300

    Add loading spinner to show while fetching

--- js/surah.js
+++ js/surah.js
@@ -2,4 +2,14 @@ const fetchingSingalSurah = async () => {
   const urlParams = new URLSearchParams(window.location.search);
   const surahNumber = urlParams.get("surah");
   const ayahContainer = document.querySelector(".ayah-container");
+
+  ayahContainer.innerHTML = `
+    <div class="container content-page">
+      <div class="center-content col">
+        <div class="text-center text--medium p-3">
+          <div class="loader"></div>
+        </div>
+      </div>
+    </div>
+  `;
 };
